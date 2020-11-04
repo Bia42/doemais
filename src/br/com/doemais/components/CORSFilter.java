8a70c43aa60ff0539ab914164632108f2e2c07ee
Beatriz Oliveira <beatrizo237@gmail.com>
2020-11-03 23:36:29 -0200

    teste

--- src/br/com/doemais/components/CORSFilter.java
+++ src/br/com/doemais/components/CORSFilter.java
@@ -18,7 +18,7 @@ public class CORSFilter implements Filter
 	        HttpServletRequest request = (HttpServletRequest) req;
 	        response.setHeader("Access-Control-Allow-Origin", "*");
 	        response.setHeader("Access-Control-Max-Age", "3600");
-	        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+	        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 	            response.setStatus(HttpServletResponse.SC_OK);
